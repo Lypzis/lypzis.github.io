91bfc214cf30b928e9a848f23e39436295b88d1c
Lypzis <victorvp2008@hotmail.com>
2019-09-17 11:50:40 -0300

    Page Fix

---
+++ precache-manifest.338056de237e67df579efc5fe8ff1dbf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360c4a7b29747110cfd2516ea141059c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a435d90627c46e63b6da",
+    "url": "/static/css/main.6005c18a.chunk.css"
+  },
+  {
+    "revision": "3deb810ba55b98fdcfba",
+    "url": "/static/js/2.b6169b81.chunk.js"
+  },
+  {
+    "revision": "a435d90627c46e63b6da",
+    "url": "/static/js/main.10e65d8a.chunk.js"
+  },
+  {
+    "revision": "748761b4f060bca03b5a",
+    "url": "/static/js/runtime~main.30c55c12.js"
+  },
+  {
+    "revision": "930efd9d736e3fb4f1c2a169721c2233",
+    "url": "/static/media/Castle-bg-small.930efd9d.jpg"
+  },
+  {
+    "revision": "1314715349a05d17eb2f37ce3415a398",
+    "url": "/static/media/Castle-bg.13147153.jpg"
+  },
+  {
+    "revision": "1808d0a8ae717067046b2ba3eb3597b8",
+    "url": "/static/media/OpenBook-small.1808d0a8.jpg"
+  },
+  {
+    "revision": "5e3eb9476b3d1f7f5ef3872e0c5fd875",
+    "url": "/static/media/OpenBook.5e3eb947.jpg"
+  },
+  {
+    "revision": "127b470a1814ca2ae06d9f40df34f190",
+    "url": "/static/media/React.127b470a.png"
+  },
+  {
+    "revision": "8d3ab795086cc892c9b0ed266ea229cb",
+    "url": "/static/media/forkify-picture.8d3ab795.jpg"
+  },
+  {
+    "revision": "1a690866282b4d04a0a004fd0777aca5",
+    "url": "/static/media/lypzis-picture.1a690866.jpg"
+  },
+  {
+    "revision": "a6c92868331881b92cecd6dc13683e83",
+    "url": "/static/media/mind-blowing.a6c92868.gif"
+  },
+  {
+    "revision": "ae25a1f3ca92bbfb26217c28befacb65",
+    "url": "/static/media/natour-picture.ae25a1f3.jpg"
+  },
+  {
+    "revision": "31e65686540d5e760f970548726f8ed2",
+    "url": "/static/media/omnifood-picture.31e65686.jpg"
+  },
+  {
+    "revision": "02464fc7bddddbb48294e5d72b70869e",
+    "url": "/static/media/trillo-picture.02464fc7.jpg"
+  }
+]);
